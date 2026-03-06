18ca477dfe94778dd051dfaeb8251112622f43d2
Mohammed Bouali <68650187+MedBouali@users.noreply.github.com>
2026-03-06 06:41:10 +0000

    feat: add main layout and hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,29 @@
 export default function Hero() {
     return (
-        <></>
+        <section className="hero">
+            <h1>Track Your Caffeine, Master Your Energy <i class="fa-solid fa-mug-hot"></i></h1>
+
+            <div className="benefits-list">
+                <h3>
+                    Use <strong>CaffeineLog</strong> to start...
+                </h3>
+                <p><i className="fa-solid fa-check"></i> Logging every coffee, tea, or energy drink</p>
+                <p><i className="fa-solid fa-check"></i> Tracking caffeine levels in your body over time</p>
+                <p><i className="fa-solid fa-check"></i> Understanding your daily caffeine habits</p>
+            </div>
+
+            <div className="info-section">
+                <h3>
+                    <i className="fa-solid fa-info"></i> Did you know?
+                </h3>
+                <h5>Caffeine stays in your system longer than you think.</h5>
+                <p>
+                Caffeine has an average half-life of about 5 hours. That means
+                five hours after drinking coffee, about half of the caffeine is
+                still active in your body. For example, if you drink 200 mg of
+                caffeine, roughly 100 mg may still be affecting you five hours later.
+                </p>
+            </div>
+        </section> 
     )
 }
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,7 +1,18 @@
 export default function Layout(props) {
     const { children } = props
-    const header = <header></header>
-    const footer = <footer></footer>
+    const header = <header className="header">
+        <div>
+            <h1>CaffeineLog</h1>
+            <p>Track your caffeine. Understand your habits.</p>
+        </div>
+        <button className="btn btn-primary">
+            <i className="fa-solid fa-arrow-right-to-bracket"></i>
+            Login
+        </button>
+    </header>
+    const footer = <footer className="footer">
+        <p><strong>CaffeineLog</strong> was made by <a href="https://github.com/MedBouali" target="_blank" rel="noopener noreferrer">Mohammed Bouali</a>.</p>
+    </footer>
 
     return (
         <>
